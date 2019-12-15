7a9a93020fdb4002763725967a3a40d2d9369ed4
Arbort <781642016@qq.com>
2019-12-15 22:26:04 +0800

    修改路由错误时统一返回/路径

--- src/router.js
+++ src/router.js
@@ -50,10 +50,6 @@ export default new Router({
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ './views/mailbox/index.vue')
-    },
-    {
-        path:"*",
-        redirect:'/'
     }
   ]
 })
